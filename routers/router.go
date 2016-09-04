82db46ac5eda433a42aed676c7faf0286f9c6c99
Lenfree Yeung <lenfree.yeung@gmail.com>
2016-09-04 15:13:21 +1000

    Initial commit

---
+++ routers/router.go
@@ -0,0 +1,28 @@
+// @APIVersion 1.0.0
+// @Title Barcode API
+// @Description Barcode
+// @Contact lenfree.yeung@gmail.com
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+        "github.com/lenfree/barcode/controllers"
+        "github.com/astaxie/beego"
+)
+
+func init() {
+        ns := beego.NewNamespace("/v1",
+                beego.NSNamespace("/object",
+                        beego.NSInclude(
+                                &controllers.ObjectController{},
+                        ),
+                ),
+                beego.NSNamespace("/user",
+                        beego.NSInclude(
+                                &controllers.UserController{},
+                        ),
+                ),
+        )
+        beego.AddNamespace(ns)
+}
