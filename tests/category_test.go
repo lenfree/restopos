5b51ff426e8f986ede63d1814e7bbec2757c88c8
Lenfree Yeung <lenfree.yeung@gmail.com>
2016-09-07 19:25:45 +1000

    * Add stub for category post, get, get by ID instead of writing to test DB

--- tests/category_test.go
+++ tests/category_test.go
@@ -2,77 +2,92 @@ package test
 
 import (
     "encoding/json"
-    "net/http"
-    "net/http/httptest"
     "testing"
-    "github.com/lenfree/barcode/models"
     _ "github.com/lenfree/barcode/routers"
+    "io/ioutil"
     "strings"
 
-    "github.com/astaxie/beego"
     . "github.com/smartystreets/goconvey/convey"
+    "github.com/nicholasf/fakepoint"
+    "github.com/lenfree/barcode/models"
 )
 
 func TestPostCategory(t *testing.T) {
+    data, _ := json.Marshal(models.Category{ID: 1, Name: "Drinks"})
+
+    maker := fakepoint.NewFakepointMaker()
+    fakepoint := maker.NewPost("http://localhost:8080/v1/category", 200)
+    fakepoint.SetResponse(string(data)).SetHeader("Content-Type", "application/json")
+
     reqData := `{"category_name": "Drinks"}`
     reqBody := strings.NewReader(reqData)
 
-    r, _ := http.NewRequest("POST", "/v1/category", reqBody)
-    r.Header.Set("Content-Type", "application/json")
-    w := httptest.NewRecorder()
-    beego.BeeApp.Handlers.ServeHTTP(w, r)
-
-    beego.Trace("testing", "TestPostCategory", "Code[%d]\n%s", w.Code, w.Body.String())
-
-    var response struct {
-        ID        int `json:"category_id"`
-        Name      string `json:"category_name"`
-    }
-
-    json.Unmarshal(w.Body.Bytes(), &response)
+    resp, _ := maker.Client().Post("http://localhost:8080/v1/category", "application/json", reqBody)
+    body, _ := ioutil.ReadAll(resp.Body)
 
     Convey("Subject: Create category\n", t, func() {
             Convey("Status Code Should Be 200", func() {
-                    So(w.Code, ShouldEqual, 200)
+                    So(resp.StatusCode, ShouldEqual, 200)
             })
-            Convey("Return last inserted ID", func() {
-                    So(response.ID, ShouldEqual, 1)
-            })
-            Convey("Return last inserted category name", func() {
-                    So(response.Name, ShouldEqual, "Drinks")
+            Convey("Status Code return ID:1 and Name: Drinks", func() {
+                    So(string(body), ShouldEqual, string(data))
             })
     })
 }
 
 func TestGetCategory(t *testing.T) {
-    reqData := `{"category_name": "Drinks"}`
-    reqBody := strings.NewReader(reqData)
+    data, _ := json.Marshal([]models.Category{
+            {ID: 1, Name: "App"},
+            {ID: 2, Name: "Drinks"},
+    })
 
-    rq, _ := http.NewRequest("POST", "/v1/category", reqBody)
-    rq.Header.Set("Content-Type", "application/json")
+    maker := fakepoint.NewFakepointMaker()
+    fakepoint := maker.NewGet("http://localhost:8080/v1/category", 200)
+    fakepoint.SetResponse(string(data)).SetHeader("Content-Type", "application/json")
 
-    r, _ := http.NewRequest("GET", "/v1/category", nil)
-    w := httptest.NewRecorder()
-    beego.BeeApp.Handlers.ServeHTTP(w, r)
-
-    beego.Trace("testing", "TestGetCategory", "Code[%d]\n%s", w.Code, w.Body.String())
+    resp, _ := maker.Client().Get("http://localhost:8080/v1/category")
+    body, _ := ioutil.ReadAll(resp.Body)
 
     var response []*models.Category
-    _ = json.Unmarshal(w.Body.Bytes(), &response)
-    user := &models.Category{
-      ID: 1,
-      Name: "Drinks",
-    }
+    _ = json.Unmarshal(body, &response)
 
-    Convey("Subject: Get Category\n", t, func() {
+    Convey("Subject: Create category\n", t, func() {
             Convey("Status Code Should Be 200", func() {
-                    So(w.Code, ShouldEqual, 200)
+                    So(resp.StatusCode, ShouldEqual, 200)
             })
-            Convey("should not return nil", func() {
-                    So(w.Body.Len(), ShouldBeGreaterThan, 0)
+            Convey("should return category all category", func() {
+                    So(string(body), ShouldEqual, string(data))
             })
-            Convey("should return category name", func() {
-                    So(response[0], ShouldResemble, user)
+            Convey("should category count equal 2", func() {
+                    So(len(response), ShouldEqual, 2)
+            })
+    })
+}
+
+func TestGetCategoryID(t *testing.T) {
+    data, _ := json.Marshal([]models.Category{
+            {ID: 2, Name: "App"},
+    })
+
+    maker := fakepoint.NewFakepointMaker()
+    fakepoint := maker.NewGet("http://localhost:8080/v1/category/1", 200)
+    fakepoint.SetResponse(string(data)).SetHeader("Content-Type", "application/json")
+
+    resp, _ := maker.Client().Get("http://localhost:8080/v1/category/1")
+    body, _ := ioutil.ReadAll(resp.Body)
+
+    var response []*models.Category
+    _ = json.Unmarshal(body, &response)
+
+    Convey("Subject: Create category\n", t, func() {
+            Convey("Status Code Should Be 200", func() {
+                    So(resp.StatusCode, ShouldEqual, 200)
+            })
+            Convey("should return category all category", func() {
+                    So(string(body), ShouldEqual, string(data))
+            })
+            Convey("should category count equal 2", func() {
+                    So(len(response), ShouldEqual, 1)
             })
     })
 }
--- tests/helper_test.go
+++ tests/helper_test.go
@@ -5,7 +5,9 @@ import (
     "runtime"
     "path/filepath"
     _ "github.com/lenfree/barcode/routers"
+    "net/http"
     "os"
+    "strings"
 
     "github.com/astaxie/beego"
     _ "github.com/mattn/go-sqlite3"
@@ -32,7 +34,17 @@ func init() {
 func tearDown() {
     beego.Debug("Starting teardown")
     _, file, _, _ := runtime.Caller(1)
-    apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".."+string(filepath.Separator))))
+    apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".." + string(filepath.Separator))))
     db := apppath + string(filepath.Separator) + "my_test.db"
     os.Remove(db)
+}
+
+func setup() {
+    beego.Debug("Load data")
+    reqData := `{"category_name": "Drinks3"}`
+    reqBody := strings.NewReader(reqData)
+
+    r, _ := http.NewRequest("POST", "/v1/category", reqBody)
+    r.Header.Set("Content-Type", "application/json")
+
 }
